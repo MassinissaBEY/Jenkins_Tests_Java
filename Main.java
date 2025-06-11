e8d50a8502769dcd119bddcc8eb599b0d59833ec
MassinissaBEY <104314110+MassinissaBEY@users.noreply.github.com>
2025-06-11 10:40:45 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    if(args[(args.length-1)].equals("+")){
+    if (args[(args.length - 1)].equals("+")) {
       System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
-    }
-    else if(args[(args.length-1)].equals("-")){
+    } else if (args[(args.length - 1)].equals("-")) {
       System.out.println(Integer.parseInt(args[0]) - Integer.parseInt(args[1]));
-    }
-    else if(args[(args.length-1)].equals("*")){
+    } else if (args[(args.length - 1)].equals("*")) {
       System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
-    }
-    else if(args[(args.length-1)].equals("/")){
-      System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
-    }
-    else {
+    } else if (args[(args.length - 1)].equals("/")) {
+      if (Integer.parseInt(args[1]) != 0) { 
+        System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
+      } else {
+        System.out.println("Division par zéro interdite");
+      }
+    } else {
       System.exit(-1);
     }
   }
